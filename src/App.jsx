80983895e53803ad460f91a580dad0ba6ae5cc61
Prashant Lekhwar <lekhwarprashant@gmail.com>
2025-05-23 23:19:12 +0530

    3D portfolio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero from "./sections/hero";
+
+const App = () => {
+    return(
+        <Hero />
+        
+    )
+}
+export default App
+
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hero(){
+    return(
+        <section id='hero' className='realtive hidden'>
+            <div className='absolute top-0 left-0 z-10'>
+                <img src="/images/bg.png" alt="background" />
+            </div>
+
+        </section>
+        
+    );
+}
+export default Hero
