4312663a6a313790972e5976878454cdb75333b7
Prashant Lekhwar <lekhwarprashant@gmail.com>
2025-05-26 22:47:51 +0530

    hero 1st section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 
+const words = [
+    {text: 'Ideas', image: '/images/ideas.svg'}
+]
+
 function Hero(){
     return(
         <section id='hero' className='realtive hidden'>
             <div className='absolute top-0 left-0 z-10'>
                 <img src="/images/bg.png" alt="background" />
+            </div>
+
+            <div className='hero-layout' >
+                {/* Left hero content  for text*/}
+                <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5' >
+                    <div className='flex flex-col gap-7'>
+                        <div className='hero-text'>
+                            <h1>
+                                Shaping
+                                <span className='slider'>
+                                    <span className='wrapper'>
+                                        {words.map((word) =>
+                                            (<span key={word.text} className='flex items-center md: gap-3 gap-1'>
+                                                <img
+                                                 src={word.image}
+                                                 alt={word.text}
+                                                 className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'/>
+                                                 <span>{word.text}</span>
+                                            </span>)
+                                        )}
+                                    </span>
+                                </span>
+
+                            </h1>
+                            <h1>into Real Projects</h1>
+                            <h1>that Delivers Results</h1>
+                        </div>
+                    </div>
+                </header>
+                {/* Right hero section for 3D model */}
             </div>
 
         </section>
