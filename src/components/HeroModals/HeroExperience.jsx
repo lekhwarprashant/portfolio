2ccc9450557035b419c1903725d3e0e6c20ddc8d
Prashant Lekhwar <lekhwarprashant@gmail.com>
2025-06-16 23:35:15 +0530

    new 3D model

--- src/components/HeroModals/HeroExperience.jsx
+++ src/components/HeroModals/HeroExperience.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Canvas} from '@react-three/fiber';
 import {OrbitControls} from '@react-three/drei';
 import {useMediaQuery} from 'react-responsive';
+import { Room } from './Room';
 
 function HeroExperience(){
     const isTablet = useMediaQuery({query: '(max-width: 1024px)'});
@@ -9,21 +10,23 @@ function HeroExperience(){
     return(
         <Canvas camera={{position:[0,0,15], fov:45}}>
             <ambientLight intensity={0.2} color='#1a1a40' />
-            <directionalLight position={[5,5,5]} intensity={5} />
+            <directionalLight position={[5,5,5]} intensity={2} />
 
             <OrbitControls
             enablePan= {false}
-            enableZoom= {isTablet}
+            enableZoom= {!isTablet}
             maxDistance={20}
             minDistance= {5}
             minPolarAngle= {Math.PI /5}
             maxPolarAngle= {Math.PI/2}
             />
-
-            <mesh>
-                <boxGeometry args={[1,1,1]} />
-                <meshStandardMaterial color='teal'/>
-            </mesh>
+            <group
+                scale={isTablet? 0.7 : 1}
+                position={[0, -3.5, 0]}
+                rotation={[0, -Math.PI/4, 0]}
+            >
+                <Room/>
+            </group>
 
         </Canvas>
     );
