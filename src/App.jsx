2bb4002dc0fee087f49b1e8558c72eb563e5016b
Prashant Lekhwar <lekhwarprashant@gmail.com>
2025-09-25 07:34:24 +0530

    Mainly everything done. Now deployment required

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import NavBar from "./components/NavBar";
 import LogoShowcase from "./sections/LogoShowcase";
 import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
+import TechStack from "./sections/TechStack";
+import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 const App = () => {
     return(
@@ -15,6 +19,10 @@ const App = () => {
             <LogoShowcase/>
             <FeatureCards/>
             <ExperienceSection/>
+            <TechStack/>
+            <Testimonials/>
+            <Contact/>
+            <Footer/>
         </>
     )
 }
--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -6,12 +6,22 @@ function GlowCard({card, children, index}){
     const cardRefs = useRef([]);
     const handleMouseMove = (index) => (e) => {
         const card = cardRefs.current[index];
-        if(card)
+        if(!card)
             return ;
+
+        const rect = card.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left - rect.width/2;
+        const mouseY = e.clientY - rect.top - rect.height/2;
+
+        let angle = Math.atan2(mouseY, mouseX)*(180/Math.PI);
+
+        angle = (angle + 360)%360;
+
+        card.style.setProperty('--start', angle+ 60);
     }
  
     return(
-        <div ref={(el)=>(cardRefs.current[index] = el)} onMouseMove={handleMouseMove(index)}  className='card card-border timeline-card rounded-xl p-10'>
+        <div ref={(el)=>(cardRefs.current[index] = el)} onMouseMove={handleMouseMove(index)}  className='card card-border timeline-card rounded-xl p-10 mb-5 break-inside-avoid-column'>
             <div className='glow'/>
             <div className='flex items-center gap-1 mb-5'>
                 {Array.from({length:5},(_,i)=>(
